f37bf6868e79cbb6d0e2d55ec031e7a442d0ddeb
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-15 00:40:22 +0000

    Add onto the getFoodData function. loop through the response of the request to create a DOM tree for each result that includes the image and food label. append to the row class above.  Remove the dummy search results.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 
-// Clear Search Bar
-
 var $cancelIcon = document.querySelector('#cancelicon');
 var $searchText = document.querySelector('#searchbar');
 var $formSubmit = document.querySelector('#form');
+var $rowResult = document.querySelector('.result-item');
 
-$cancelIcon.addEventListener('click', clearSearch);
-
+// Clear Search Bar
 function clearSearch(event) {
   $searchText.value = '';
 }
+$cancelIcon.addEventListener('click', clearSearch);
 
 // HTTP Request
 
@@ -21,9 +20,33 @@ function getFoodData(event) {
   xhr.open('GET', 'https://api.edamam.com/api/food-database/v2/parser?app_id=62e1382f&app_key=fb581bd2de03e8a30b53d8a1a76b8b79&ingr=' + $searchText.value);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log(xhr.status);
-    // console.log(xhr.response);
+    for (var i = 0; i < xhr.response.hints.length; i++) {
+      var result = renderResult(xhr.response.hints[i]);
+      $rowResult.appendChild(result);
+    }
   });
   xhr.send();
   $formSubmit.reset();
+}
+
+function renderResult(result) {
+  var $newDiv = document.createElement('div');
+  $newDiv.setAttribute('class', 'column-sixth flex-column');
+
+  var $imageDiv = document.createElement('div');
+  $imageDiv.setAttribute('class', 'image-wrapper');
+
+  $newDiv.appendChild($imageDiv);
+
+  var $newImg = document.createElement('img');
+  $newImg.setAttribute('src', result.food.image);
+
+  $imageDiv.appendChild($newImg);
+
+  var $newH4 = document.createElement('h4');
+  $newH4.textContent = result.food.label;
+
+  $newDiv.appendChild($newH4);
+
+  return $newDiv;
 }
