9300d987260d56bd05b7fde8806263aca0c559c1
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-16 19:54:56 +0000

    reworked js code to remove unnecessary [0] index for resultsArray.

--- js/main.js
+++ js/main.js
@@ -27,9 +27,10 @@ function getFoodData(event) {
       $noResults.setAttribute('class', 'row noresults');
     } else {
       $rowResult.replaceChildren();
-      resultsArray.push(xhr.response.hints);
-      for (var i = 0; i < resultsArray[0].length; i++) {
-        var result = renderResult(resultsArray[0][i].food);
+      resultsArray = [];
+      resultsArray = [...xhr.response.hints];
+      for (var i = 0; i < resultsArray.length; i++) {
+        var result = renderResult(resultsArray[i].food);
         result.setAttribute('data-search-index', i);
         $rowResult.appendChild(result);
         viewSwap('results-page');
@@ -86,8 +87,11 @@ function clickFunction(event) {
   var pageView = event.target.getAttribute('data-view');
   viewSwap(pageView);
   $noResults.setAttribute('class', 'row noresults hidden');
-  resultsArray = [];
   data.view = pageView;
+  if (pageView === 'favorites-page') {
+    resultsArray = [];
+    resultsArray.push(data.favorites);
+  }
 }
 $searchLink.addEventListener('click', clickFunction);
 $favoriteLink.addEventListener('click', clickFunction);
@@ -100,22 +104,23 @@ var $detailsProtein = document.querySelector('#details-protein');
 var $detailsCarbs = document.querySelector('#details-carbs');
 var $detailsFat = document.querySelector('#details-fat');
 var $resultsNodes = document.querySelector('.result-item');
+var $favoritesNodes = document.querySelector('.favorite-items');
 
 function clickDetails(event) {
   if (event.target.tagName === 'IMG' || event.target.tagName === 'H4') {
-    for (var j = 0; j < resultsArray[0].length; j++) {
+    for (var j = 0; j < resultsArray.length; j++) {
       var closestDiv = event.target.closest('div[data-search-index]');
       if (closestDiv.getAttribute('data-search-index') === j.toString()) {
-        if (resultsArray[0][j].food.image !== undefined) {
-          $detailsImg.setAttribute('src', resultsArray[0][j].food.image);
+        if (resultsArray[j].food.image !== undefined) {
+          $detailsImg.setAttribute('src', resultsArray[j].food.image);
         } else {
           $detailsImg.setAttribute('src', 'images/MacVueIcon.png');
         }
-        $detailsHeader.textContent = resultsArray[0][j].food.label;
-        $detailsKcal.textContent = Math.floor(resultsArray[0][j].food.nutrients.ENERC_KCAL);
-        $detailsProtein.textContent = Math.floor(resultsArray[0][j].food.nutrients.PROCNT);
-        $detailsCarbs.textContent = Math.floor(resultsArray[0][j].food.nutrients.CHOCDF);
-        $detailsFat.textContent = Math.floor(resultsArray[0][j].food.nutrients.FAT);
+        $detailsHeader.textContent = resultsArray[j].food.label;
+        $detailsKcal.textContent = Math.floor(resultsArray[j].food.nutrients.ENERC_KCAL);
+        $detailsProtein.textContent = Math.floor(resultsArray[j].food.nutrients.PROCNT);
+        $detailsCarbs.textContent = Math.floor(resultsArray[j].food.nutrients.CHOCDF);
+        $detailsFat.textContent = Math.floor(resultsArray[j].food.nutrients.FAT);
       }
     }
     viewSwap('details-page');
@@ -123,6 +128,7 @@ function clickDetails(event) {
 }
 
 $resultsNodes.addEventListener('click', clickDetails);
+$favoritesNodes.addEventListener('click', clickDetails);
 
 // save to favorite function
 var $rowFavorite = document.querySelector('.favorite-items');
@@ -134,16 +140,21 @@ var heart = false;
 function saveToFavorite(event) {
   if (heart === false) {
     // save to local storage
-    var favoriteObject = {};
-    favoriteObject.label = $detailsHeader.textContent;
-    favoriteObject.image = $detailsImg.src;
-    favoriteObject.kcal = $detailsKcal.textContent;
-    favoriteObject.protein = $detailsProtein.textContent;
-    favoriteObject.carbs = $detailsCarbs.textContent;
-    favoriteObject.fat = $detailsFat.textContent;
+    var favoriteObject = {
+      food: {
+        label: $detailsHeader.textContent,
+        image: $detailsImg.src,
+        nutrients: {
+          CHOCDF: $detailsCarbs.textContent,
+          ENERC_KCAL: $detailsKcal.textContent,
+          FAT: $detailsFat.textContent,
+          PROCNT: $detailsProtein.textContent
+        }
+      }
+    };
     data.favorites.push(favoriteObject);
     // render to favorites page
-    var favorite = renderResult(favoriteObject);
+    var favorite = renderResult(favoriteObject[0]);
     $rowFavorite.appendChild(favorite);
     // switch heart icon and boolean
     $favoriteIcon.setAttribute('class', 'fa-solid fa-heart');
@@ -159,6 +170,7 @@ function saveToFavorite(event) {
 function DOMContentLoaded(event) {
   for (var k = 0; k < data.favorites.length; k++) {
     var favorites = renderResult(data.favorites[k]);
+    favorites.setAttribute('data-search-index', k);
     $rowFavorite.appendChild(favorites);
   }
   viewSwap(data.view);
